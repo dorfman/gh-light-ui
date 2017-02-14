32b6782199e3f50a5a9747d177c3ec3481205d0a
Henry Dorfman <henrydorfman@gmail.com>
2017-02-13 21:38:59 -0800

    Tweaked colors on various css classes

--- gh-light-ui.js
+++ gh-light-ui.js
@@ -2,7 +2,21 @@
 
 function lightUI() {
   setTimeout(function() {
-    document.querySelector('.header').classList.remove('header-dark');
+    document.querySelector('.header').style['background-color'] = 'white';
+    document.querySelector('.header-logo-invertocat').style['background-color'] = '#4078c0';
+    document.querySelector('.header-search-scope').style.color = 'black';
+
+    var cl = document.querySelector('.header-search-input').classList;
+    for(var i = 0; i < cl.length; i++){
+      if (cl[i] !== 'form-control') {
+        cl.remove(cl[i]);
+      }
+    }
+
+    var el = document.querySelectorAll('.header-nav-link');
+    for(i = 0; i < el.length; i++) {
+      el[i].style.color = 'black';
+    }
   }, 1);
 }
 
